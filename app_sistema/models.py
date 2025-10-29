8e99e3f77b49bbb1155c6126061f1278fbe2c800
13668497443 <caio13483@gmail.com>
2025-10-29 16:24:47 -0300

    feat: Criar models para entidades do sistema

--- app_sistema/models.py
+++ app_sistema/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    contato = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+class Medico(models.Model):
+    nome = models.CharField(max_lenght=100)
+    numero = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
+class Consulta(models.Model):
+    STATUS_CHOICES = [
+        ('AG', 'Agendada'),
+        ('RE', 'Realizada'),
+        ('CA', 'Cancelada'),
+    ]
+
+    paciente = models.ForeignKey('Paciente', on_delete=models.CASCADE)
+    medico = models.ForeignKey('Medico', on_delete=models.CASCADE)
+    data = models.DateTimeField()
+    situacao = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AG')
+
+    def __str__(self):
+        return f"{self.paciente.nome} - {self.medico.nome} ({self.get_situacao_display()})"
