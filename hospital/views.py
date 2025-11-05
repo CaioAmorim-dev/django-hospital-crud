76bd1109dd4b5d7ea3ab5f543f344af9406e7584
boniek-júnior <boniek.junior.123@gmail.com>
2025-11-05 16:25:27 -0300

    Atualizando submódulo

--- hospital/views.py
+++ hospital/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"s /home.html")
+
+def listar_pacientes(request):
+    pacientes = Paciente.objects.all()
+    return render(request, 'pacientes/listar.html', {'pacientes': pacientes})
