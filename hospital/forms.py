ed1b1a438cbeb21aa7bcddca11904051ed498b56
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-11-05 22:59:05 -0300

    feat: Criar estilização simples para Consultaform

--- hospital/forms.py
+++ hospital/forms.py
@@ -27,3 +27,9 @@ class ConsultaForm(forms.ModelForm):
     class Meta:
         model = Consulta
         fields = ['paciente', 'medico', 'data', 'situacao']
+        widgets = {
+            'paciente': forms.Select(attrs={'class': 'form-select'}),
+            'medico': forms.Select(attrs={'class': 'form-select'}),
+            'data': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'situacao': forms.Select(attrs={'class': 'form-select'}),
+        }
