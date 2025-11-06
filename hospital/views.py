fe46bc93966e54b671e64035ab234d7e5c50e3fb
boniek-júnior <boniek.junior.123@gmail.com>
2025-11-06 16:39:00 -0300

    feat: Adicionando as funções criar, listar, editar e excluir pacientes.

--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Paciente
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,29 @@ def home(request):
 def listar_pacientes(request):
     pacientes = Paciente.objects.all()
     return render(request, 'pacientes/listar.html', {'pacientes': pacientes})
+
+def criar_pacientes(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        idade = request.POST['idade']
+        contato = request.POST['contato']
+        Paciente.objects.create(nome=nome, idade=idade, contato=contato)
+        return redirect('listar_pacientes')
+    return render(request, 'pacientes/criar.html')
+
+def editar_pacientes(resquest, id):
+    paciente = get_object_or_404(Paciente, id=id)
+    if resquest.method == 'POST':
+        paciente.nome = resquest.POST['nome']
+        paciente.idade = resquest.POST['idade']
+        paciente.contato = resquest.POST['contato']
+        paciente.save()
+        return redirect('listar_pacientes')
+    return render(resquest, 'pacientes/editar.html', {'paciente': paciente})
+
+def excluir_pacientes(request, id):
+    paciente = get_object_or_404(Paciente, id=id)
+    if request.method == 'POST':
+        paciente.delete()
+        return redirect('listar_pacientes')
+    return render(request, 'pacientes/excluir.html', {'paciente': paciente})
