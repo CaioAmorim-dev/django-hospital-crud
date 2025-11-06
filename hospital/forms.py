5cd5731a18430c4a10adba1e6ccafc0b261af84b
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-11-05 22:54:17 -0300

    feat: Criar estilização simples para PacienteForm

--- hospital/forms.py
+++ hospital/forms.py
@@ -1,10 +1,17 @@
 from django import forms 
 from .models import Paciente, Medico, Consulta
 
-class pacienteForm(forms.ModelForm):
+class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
         fields = ["nome","idade", "contato", "situacao"]
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o nome do paciente'}),
+            'idade': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Idade'}),
+            'contato': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Telefone ou e-mail'}),
+            'situacao': forms.Select(attrs={'class': 'form-select'}),
+            }
+            
 
 class MedicoForm(forms.ModelForm):
     class Meta:
