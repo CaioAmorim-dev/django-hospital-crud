a8c482a7117d74ada2ec560d364a0c0952d95330
13668497443 <caio13483@gmail.com>
2025-11-18 13:25:12 -0300

    refactor: Aplicar novas rotas para views

--- hospital/views.py
+++ hospital/views.py
@@ -1,145 +1,185 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Paciente
-from .forms import PacienteForm
-from .models import Medico
-from .forms import MedicoForm
-from .models import Consulta
-from .forms import ConsultaForm
 from django.db.models import Q
 
-# Create your views here.
+from .models import Paciente, Medico, Consulta
+from .forms import PacienteForm, MedicoForm, ConsultaForm
+
+
+# HOME DO SISTEMA 
+
 def home(request):
     return render(request, 'home.html')
 
-def pacientes_home(request):
-    return render(request, 'pacientes/home.html')
 
-def medicos_home(request):
-    return render(request, 'medicos/home.html')
+# PACIENTE 
 
-def consultas_home(request):
-    return render(request, 'consultas/home.html')
-
-def buscar(request):
+def paciente_home(request):
     termo = request.GET.get('q', '')
 
-    pacientes = Paciente.objects.filter(
-        Q(nome__icontains=termo) |
-        Q(cpf__icontains=termo) |
-        Q(telefone__icontains=termo)
-    ) if termo else []
-
-    medicos = Medico.objects.filter(
-        Q(nome__icontains=termo) |
-        Q(especialidade__icontains=termo) |
-        Q(crm__icontains=termo)
-    ) if termo else []
-
-    contexto = {
-        'termo': termo,
-        'pacientes': pacientes,
-        'medicos': medicos,
-    }
-    return render(request, 'buscar.html', contexto)
-
-def listar_pacientes(request):
     pacientes = Paciente.objects.all()
-    return render(request, 'pacientes/listar_pacientes.html', {'pacientes': pacientes})
 
-def criar_pacientes(request):
+    if termo:
+        pacientes = pacientes.filter(
+            Q(nome__icontains=termo) |
+            Q(cpf__icontains=termo) |
+            Q(telefone__icontains=termo)
+        )
+
+    return render(request, 'paciente/home_paciente.html', {
+        'termo': termo,
+        'pacientes': pacientes
+    })
+
+
+def criar_paciente(request):
     if request.method == 'POST':
         form = PacienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_pacientes')
+            return redirect('paciente_home')
     else:
         form = PacienteForm()
-    return render(request, 'pacientes/cria_paciente.html', {"form": form})
 
-def editar_pacientes(request, id):
+    return render(request, 'paciente/criar_paciente.html', {'form': form})
+
+
+def editar_paciente(request, id):
     paciente = get_object_or_404(Paciente, id=id)
+
     if request.method == 'POST':
         form = PacienteForm(request.POST, instance=paciente)
         if form.is_valid():
             form.save()
-            return redirect('listar_pacientes')
-    else: 
+            return redirect('paciente_home')
+    else:
         form = PacienteForm(instance=paciente)
-    return render(request, 'pacientes/editar_pacientes.html', {'form': form})
 
-def excluir_pacientes(request, id):
+    return render(request, 'paciente/editar_paciente.html', {
+        'form': form,
+        'paciente': paciente
+    })
+
+
+def excluir_paciente(request, id):
     paciente = get_object_or_404(Paciente, id=id)
+
     if request.method == 'POST':
         paciente.delete()
-        return redirect('listar_pacientes')
-    return render(request, 'pacientes/excluir_pacientes.html', {'paciente': paciente})
+        return redirect('paciente_home')
 
-def listar_medicos(request):
+    return render(request, 'paciente/excluir_paciente.html', {'paciente': paciente})
+
+
+# MEDICO 
+
+def medico_home(request):
+    termo = request.GET.get('q', '')
+
     medicos = Medico.objects.all().order_by('nome')
-    return render(request, 'clinica/listar_medico.html', {'medicos': medicos})
+
+    if termo:
+        medicos = medicos.filter(
+            Q(nome__icontains=termo) |
+            Q(especialidade__icontains=termo) |
+            Q(crm__icontains=termo)
+        )
+
+    return render(request, 'medico/home_medico.html', {
+        'termo': termo,
+        'medicos': medicos
+    })
+
 
 def criar_medico(request):
     if request.method == 'POST':
         form = MedicoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_medico')
-        
+            return redirect('medico_home')
     else:
         form = MedicoForm()
-    return render(request, 'clinica/criar_medico.html', {'form': form})
+
+    return render(request, 'medico/criar_medico.html', {'form': form})
+
 
 def editar_medico(request, medico_id):
     medico = get_object_or_404(Medico, id=medico_id)
+
     if request.method == 'POST':
         form = MedicoForm(request.POST, instance=medico)
         if form.is_valid():
             form.save()
-            return redirect('listar_medico')
-        
-    else:  
+            return redirect('medico_home')
+    else:
         form = MedicoForm(instance=medico)
-    return render(request, 'clinica/editar_medico.html',{
-        'form':form,
+
+    return render(request, 'medico/editar_medico.html', {
+        'form': form,
         'medico': medico
     })
 
+
 def excluir_medico(request, medico_id):
     medico = get_object_or_404(Medico, id=medico_id)
+
     if request.method == 'POST':
         medico.delete()
-        return redirect('listar_medico')
-    
-    return render(request, 'clinica/excluir_medico.html', {'medico': medico})
+        return redirect('medico_home')
 
-def listar_consultas(request):
+    return render(request, 'medico/excluir_medico.html', {'medico': medico})
+
+
+#CONSULTA
+
+def consulta_home(request):
+    termo = request.GET.get('q', '')
+
     consultas = Consulta.objects.all()
-    return render(request, 'consultas/listar_consultas.html', {'consultas': consultas})
+
+    if termo:
+        consultas = consultas.filter(
+            Q(motivo__icontains=termo) |
+            Q(paciente__nome__icontains=termo) |
+            Q(medico__nome__icontains=termo)
+        )
+
+    return render(request, 'consulta/home_consulta.html', {
+        'termo': termo,
+        'consultas': consultas
+    })
+
 
 def criar_consulta(request):
     if request.method == 'POST':
         form = ConsultaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_consultas')
+            return redirect('consulta_home')
     else:
         form = ConsultaForm()
-    return render(request, 'consultas/criar.html', {'form': form})
+
+    return render(request, 'consulta/criar_consulta.html', {'form': form})
+
 
 def editar_consulta(request, id):
     consulta = get_object_or_404(Consulta, id=id)
+
     if request.method == 'POST':
         form = ConsultaForm(request.POST, instance=consulta)
         if form.is_valid():
             form.save()
-            return redirect('listar_consultas')
+            return redirect('consulta_home')
     else:
         form = ConsultaForm(instance=consulta)
-    return render(request, 'consultas/editar_consulta.html', {'form': form})
+
+    return render(request, 'consulta/editar_consulta.html', {'form': form})
+
 
 def excluir_consulta(request, id):
     consulta = get_object_or_404(Consulta, id=id)
+
     if request.method == 'POST':
         consulta.delete()
-        return redirect('listar_consultas')
-    return render(request, 'consultas/excluir_consulta.html', {'consulta': consulta})
+        return redirect('consulta_home')
+
+    return render(request, 'consulta/excluir_consulta.html', {'consulta': consulta})
